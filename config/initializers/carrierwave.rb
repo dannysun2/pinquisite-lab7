0356b3edc8e35c77ade4d1b57e0a65d768c8433c
Danny Sun <danny.sun@companiondxlab.com>
2016-10-22 01:44:02 -0500

    changed env variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => '2222', # THING 1
-    :aws_secret_access_key  => '3333', # THING 2
-    :region                 => 'us-east-1'
-  }
-
-  config.fog_directory  = 'wwww' # THING 3
-  config.fog_public     = true
-  config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
-end
+# CarrierWave.configure do |config|
+#   config.fog_credentials = {
+#     :provider               => 'AWS',
+#     :aws_access_key_id      => '2222', # THING 1
+#     :aws_secret_access_key  => '3333', # THING 2
+#     :region                 => 'us-east-1'
+#   }
+#
+#   config.fog_directory  = 'wwww' # THING 3
+#   config.fog_public     = true
+#   config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
+# end
